80eb4bbacb8e27a0a995c9160d8bdb6fcb35f443
SugarSyrup <tlfvm04@naver.com>
2021-07-26 22:04:50 +0900

    DevLog(2021.07.26)
    # 방해되는 백그라운드 이미지랑 명언 숨기기
    # 각 섹션과 박스들 구분하기
    # 월일 추가
    
    # Hide disturbing background image and quotes (maybe not use)
    # separating each section and each box
    # Add Month/Dates

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,21 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("p#clock");
 
+// clock to Text
 function getClock(){
     const date = new Date();
     const hours = String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
+    const Days = String(date.getDay());
+    
+    const months = ['Jan','Feb',"Mar",'Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'];
+    const Month = String(months[date.getMonth()]);
+
+    const week = ['SUN', 'MON', 'THU', 'WED', 'THR', 'FRI', 'SAT'];
+    const dayOfWeek = String(week[date.getDay()]);
 
     clock.innerText = `${hours}:${minutes}:${seconds}`;
+    dates.innerText = `${Month} ${Days} (${dayOfWeek})`;
 }
 
 setInterval(getClock, 1000);
